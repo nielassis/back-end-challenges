5e359d7235360a62a015948cf8e84a8ce0409546
nielassis <nielvitorba@gmail.com>
2025-01-05 13:59:22 -0300

    feat: add swagger documentation and validate entries with zod

--- goomer-nodejs/src/routes/produtos-restaurante/index.js
+++ goomer-nodejs/src/routes/produtos-restaurante/index.js
@@ -1,15 +1,140 @@
 const express = require("express");
 const router = express.Router();
 const produtos = require("../../controllers/produtos.controller");
+const {
+  createProdutoSchema,
+  updateProdutoSchema,
+} = require("../../schemas/produtos.schema");
 
-router.post("/", produtos.createNew);
+const validate = (schema) => (req, res, next) => {
+  try {
+    schema.parse(req.body);
+    next();
+  } catch (error) {
+    return res.status(400).json({ error: error.errors });
+  }
+};
+
+/**
+ * @swagger
+ * tags:
+ *   name: Produtos
+ *   description: gerenciamento de produtos
+ */
+
+/**
+ * @swagger
+ * /produtos:
+ *   post:
+ *     summary: cadastra um produto
+ *     tags: [Produtos]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               preco:
+ *                 type: number
+ *               restauranteId:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: produto cadastrado com sucesso
+ */
+router.post("/", validate(createProdutoSchema), produtos.createNew);
+
+/**
+ * @swagger
+ * /produtos:
+ *   get:
+ *     summary: Lista todos os produtos
+ *     tags: [Produtos]
+ *     responses:
+ *       200:
+ *         description: lista de produtos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Produtos'
+ */
 
 router.get("/", produtos.findAll);
 
+/**
+ * @swagger
+ * /produtos/{restauranteId}:
+ *   get:
+ *     summary: Busca produtos por restaurante
+ *     tags: [Produtos]
+ *     parameters:
+ *       - in: path
+ *         name: restauranteId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID do restaurante
+ *     responses:
+ *       200:
+ *         description: Produtos do restaurante
+ */
+
 router.get("/:restauranteId", produtos.findById);
 
-router.put("/:id", produtos.update);
+/**
+ * @swagger
+ * /produtos/{id}:
+ *   put:
+ *     summary: Atualiza um produto
+ *     tags: [Produtos]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID do produto
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               preco:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Produto atualizado
+ */
 
-router.delete("/:id", produtos.destroy);
+router.put("/:id", validate(updateProdutoSchema), produtos.update);
+
+/**
+ * @swagger
+ * /produtos/{id}:
+ *   delete:
+ *     summary: Exclui um produto
+ *     tags: [Produtos]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID do produto
+ *     responses:
+ *       200:
+ *         description: Produto excluído
+ */
+
+router.delete("/:id", validate(updateProdutoSchema), produtos.destroy);
 
 module.exports = router;
--- goomer-nodejs/src/routes/restaurante/index.js
+++ goomer-nodejs/src/routes/restaurante/index.js
@@ -1,15 +1,145 @@
 const express = require("express");
 const router = express.Router();
 const restaurantes = require("../../controllers/restaurantes.controller");
+const {
+  createRestaurantesSchema,
+  updateRestaurantesSchema,
+} = require("../../schemas/restaurantes.schema");
+
+const validate = (schema) => (req, res, next) => {
+  try {
+    schema.parse(req.body);
+    next();
+  } catch (error) {
+    return res.status(400).json({ error: error.errors });
+  }
+};
+
+/**
+ * @swagger
+ * tags:
+ *   name: Restaurantes
+ *   description: gerenciamento de restaurantes
+ */
+/**
+ * @swagger
+ * /restaurantes:
+ *   get:
+ *     summary: Lista todos os restaurantes (somente nome e ID)
+ *     tags: [Restaurantes]
+ *     responses:
+ *       200:
+ *         description: lista de restaurantes (somente nome e ID)
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Restaurantes'
+ */
 
 router.get("/", restaurantes.findAll);
 
+/**
+ * @swagger
+ * /restaurantes/data:
+ *   get:
+ *     summary: Lista todos os dados dos restaurantes
+ *     tags: [Restaurantes]
+ *     responses:
+ *       200:
+ *         description: lista de restaurantes com todos os dados
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Restaurantes'
+ */
+
 router.get("/data", restaurantes.findAllData);
 
-router.post("/", restaurantes.createNew);
+/**
+ * @swagger
+ * /restaurantes:
+ *   post:
+ *     summary: cadastra um restaurante
+ *     tags: [Restaurantes]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               foto:
+ *                 type: string
+ *               nome:
+ *                 type: string
+ *               horarioFuncionamento:
+ *                 type: string
+ *               endereco:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: restaurante cadastrado com sucesso
+ */
 
-router.put("/:id", restaurantes.update);
+router.post("/", validate(createRestaurantesSchema), restaurantes.createNew);
 
-router.delete("/:id", restaurantes.destroy);
+/**
+ * @swagger
+ * /restaurantes/{id}:
+ *   put:
+ *     summary: Atualiza um restaurante
+ *     tags: [Restaurantes]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID do restaurante
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               foto:
+ *                 type: string
+ *               nome:
+ *                 type: string
+ *               horarioFuncionamento:
+ *                 type: string
+ *               endereco:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: restaurante atualizado
+ */
+
+router.put("/:id", validate(updateRestaurantesSchema), restaurantes.update);
+
+/**
+ * @swagger
+ * /restaurantes/{id}:
+ *   delete:
+ *     summary: Exclui um restaurante
+ *     tags: [Restaurantes]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID do restaurante
+ *     responses:
+ *       200:
+ *         description: Restaurante excluído
+ */
+
+router.delete("/:id", validate(updateRestaurantesSchema), restaurantes.destroy);
 
 module.exports = router;

295e1e921405b60e0f6f277d9155dd0fe7e9f176
nielassis <nielvitorba@gmail.com>
2025-01-06 21:49:17 -0300

    feat: add eslint for code standardization

---
+++ ambulnz-nodejs/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["standard"],
+  parseOptions: {
+    ecmaversion: 12,
+    sourceType: "module",
+  },
+  rules: {},
+};
